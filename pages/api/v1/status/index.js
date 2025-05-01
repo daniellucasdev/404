af0c7272e51f93040c0a2136e75ff3480a9b5d58
Dan <105459960+daniellucasdev@users.noreply.github.com>
2025-05-01 12:46:30 +0000

    Retorno da API melhorado

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,27 @@ import database from "infra/database.js"
 console.log(database)
 
 async function status(request, response) {
-    const result = await database.query('SELECT 1 + 1 as sum;')
-    console.log(result.rows)
-    response.status(200).json({ chave: "Valor"})
+
+    const updatedAt = new Date().toISOString();
+
+    const postgresVersionRaw = await database.query("SELECT version();");
+    const postgresVersionString = postgresVersionRaw.rows[0].version;
+
+    const connectionsList = await database.query("SHOW max_connections");
+    const connectionObj = connectionsList.rows[0];
+
+    const usedConnections = await database.query(
+        "SELECT count(*) FROM pg_stat_activity",
+      );
+
+  const usedConnectionsCount = usedConnections.rows[0].count;
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        postgres_version: postgresVersionString,
+        max_connections: connectionObj.max_connections,
+        used_connections: usedConnectionsCount,
+    })
 }
 
 export default status;
