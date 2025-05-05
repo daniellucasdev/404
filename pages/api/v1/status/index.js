de6616c6fb00a36d8fbc9226ec687df5cd5e15cc
Dan <105459960+daniellucasdev@users.noreply.github.com>
2025-05-05 22:42:06 +0000

    Retornando conexoes maximas no endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,25 +9,17 @@ async function status(request, response) {
     const databaseVersionResult = await database.query("SHOW server_version;");
     const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-    const connectionsList = await database.query("SHOW max_connections");
-    const connectionObj = connectionsList.rows[0];
-
-    const usedConnections = await database.query(
-        "SELECT count(*) FROM pg_stat_activity",
-      );
-
-  const usedConnectionsCount = usedConnections.rows[0].count;
+const databaseMaxConnectionsResult = await database.query("SHOW max_connections")
+const databaseMaxConnectionsValue = await databaseMaxConnectionsResult.rows[0].max_connections
 
     response.status(200).json({
         updated_at: updatedAt,
         dependencies: {
             database : {
-                version: databaseVersionValue
+                version: databaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue),
             }
         },
-        postgres_version: databaseVersionValue,
-        max_connections: connectionObj.max_connections,
-        used_connections: usedConnectionsCount,
     })
 }
 
