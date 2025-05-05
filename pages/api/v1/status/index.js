367af7d965b6829d74dbcec9dfe534815e31bc8d
Dan <105459960+daniellucasdev@users.noreply.github.com>
2025-05-05 22:48:24 +0000

    Conexoes maximas excedidas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,13 +9,16 @@ async function status(request, response) {
     const databaseVersionResult = await database.query("SHOW server_version;");
     const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-const databaseMaxConnectionsResult = await database.query("SHOW max_connections")
-const databaseMaxConnectionsValue = await databaseMaxConnectionsResult.rows[0].max_connections
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+    const databaseMaxConnectionsValue = await databaseMaxConnectionsResult.rows[0].max_connections
+
+    const databaseOpenedConnectionsResult = await database.query("SELECT * pg_stat_activity;")
+    console.log(databaseOpenedConnectionsResult)
 
     response.status(200).json({
         updated_at: updatedAt,
         dependencies: {
-            database : {
+            database: {
                 version: databaseVersionValue,
                 max_connections: parseInt(databaseMaxConnectionsValue),
             }
