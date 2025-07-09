374f01753d0eef79e7f6268b5bd256d7762017d6
Dan <105459960+daniellucasdev@users.noreply.github.com>
2025-07-09 11:00:00 +0000

    23.2.4

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,4 +1,15 @@
-export default async function migrations(request, response) {
+import { up } from 'infra/migrations/1751723788491_first-migration-test'
+import migrationRunner from 'node-pg-migrate'
+import { join } from "node:path"
 
-    response.status(200).json([])
+export default async function migrations(request, response) {
+   const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations"
+   })
+    response.status(200).json(migrations)
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -4,6 +4,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
 
    const responseBody = await response.json();
    
+   console.log(responseBody)
    expect(Array.isArray(responseBody)).toBe(true)
 
 })
