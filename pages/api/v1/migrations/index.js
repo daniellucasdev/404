e903755f2eab05e57b8084dbed5a299300d15408
Dan <105459960+daniellucasdev@users.noreply.github.com>
2025-07-13 22:26:22 +0000

    Testes e migrations ajustadas

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,8 @@ import migrationRunner from 'node-pg-migrate'
 import { join } from "node:path"
 
 export default async function migrations(request, response) {
+    if (request.method === "GET"){
+
    const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
@@ -12,4 +14,19 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations"
    })
     response.status(200).json(migrations)
+    }
+
+    if (request.method === "POST"){
+         const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: false,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations"
+   })
+    response.status(200).json(migrations)
+    }
+
+    response.status(405).end();
 }
